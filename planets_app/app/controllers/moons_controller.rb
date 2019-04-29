fba244f12071b510a0425f9603efd708f903bdf8
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-29 11:23:52 -0400

    finish random

--- planets_app/app/controllers/moons_controller.rb
+++ planets_app/app/controllers/moons_controller.rb
@@ -1,2 +1,9 @@
 class MoonsController < ApplicationController
+    def index
+        @moons = Moon.all
+    end
+
+    def show
+        @moon = Moon.find(params[:id])
+    end
 end
--- planets_app/app/controllers/planets_controller.rb
+++ planets_app/app/controllers/planets_controller.rb
@@ -6,4 +6,10 @@ class PlanetsController < ApplicationController
     def show
         @planet = Planet.find(params[:id])
     end
+
+    def random
+        @planet = Planet.find(rand(8))
+        redirect_to @planet
+        # render "random.html.erb"
+    end
 end
